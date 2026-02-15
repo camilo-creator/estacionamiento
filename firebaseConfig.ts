160afd5e5ba6b902269aa5e8d013c96ef25581fb
camilo-creator <68717253+camilo-creator@users.noreply.github.com>
2026-02-15 10:09:58 -0300

    firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,10 +1,9 @@
-// ✅ Pega aquí tu config de Firebase (Web app) desde:
-// Firebase Console → Project settings ⚙️ → Your apps → Web app → Config
 export const firebaseConfig = {
-  apiKey: "PEGA_AQUI",
-  authDomain: "PEGA_AQUI",
+  apiKey: "AIzaSyDXxvBG0HuFIH5b8vpQkggtqJVJAQZca88",
+  authDomain: "estacionamiento-kw.firebaseapp.com",
+  databaseURL: "https://estacionamiento-kw-default-rtdb.firebaseio.com",
   projectId: "estacionamiento-kw",
-  storageBucket: "PEGA_AQUI",
-  messagingSenderId: "PEGA_AQUI",
-  appId: "PEGA_AQUI",
-};
+  storageBucket: "estacionamiento-kw.firebasestorage.app",
+  messagingSenderId: "474380177810",
+  appId: "1:474380177810:web:9448efb41c8682e8a4714b",
+  measurementId: "G-NCGETWMJP3"
